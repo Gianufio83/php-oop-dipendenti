20b1236ddf0a6f7088c8f4e453a236b0ba48e304
Gianufio83 <fiore.gianuario@gmail.com>
2020-03-11 18:59:09 +0100

    esercizio con dipendenti e dirigenti

--- azienda/dirigenti.php
+++ azienda/dirigenti.php
@@ -1,12 +1,23 @@
 <?php
-  require_once 'Dipendenti.php';
+  require_once 'dipendenti.php';
   class Dirigenti extends Dipendenti
   {
     private $bonus;
-    public function __construct($_matricola, $_nome, $_cognome, $_indirizzo, $_cf, $_iban, $_mansioneId, $_orari, $_bonus)
+    private $investimenti;
+    public function setBonus($_bonus)
     {
-      parent::__construct($_matricola, $_nome, $_cognome, $_indirizzo, $_cf, $_iban, $_mansioneId, $_orari);
-      $this->bonus = $_bonus;
+      if (!is_numeric($_bonus)) {
+        throw new Exception('Spiacente non hai inserito un numero');
+      } else {
+        $this->bonus = $_bonus;
+      }
+    }
+    public function getBonus()
+    {
+      return $this->bonus;
+    }
+    public function getStipendioBonus($_orario, $prezzoUnitario, $_bonus)
+    {
+      return $this->stipendio = ($_orario * $prezzoUnitario) + $_bonus;
     }
   }
-?>
--- azienda/index.php
+++ azienda/index.php
@@ -1,7 +1,22 @@
 <?php
-  require_once 'Dipendenti.php';
-  require_once 'Dirigenti.php';
-  require_once 'Amministrativo.php';
-  $direttore = new Dirigenti('fffffffff', 'mario', 'rossi', 'via la vespa', 'hhhhhhhh', 'jjjjjjjjj', 6, '9-18', 20);
-  var_dump($direttore);
+  require_once 'dipendenti.php';
+  require_once 'dirigenti.php';
+  require_once 'amministrativo.php';
+  try {
+    $dipendente_1 = new Dipendenti(12345, 'nnnnnn','ggggggg');
+  } catch (Exception $e) {
+    echo 'Errore: ' . $e->getMessage();
+  }
+  $dipendente_1->setOrario(9);
+  $dipendente_1->getStipendio($dipendente_1->getOrario(), 60);
+  var_dump($dipendente_1);
+try {
+  $dirigente_1 = new Dirigenti(125454577, 'nnnnnnggg', 'gggggggyyy');
+} catch (Exception $e) {
+  echo 'Errore: ' . $e->getMessage();
+}
+$dirigente_1->setBonus(100);
+$dirigente_1->setOrario(9);
+$dirigente_1->getStipendioBonus($dirigente_1->getOrario(), 70, $dirigente_1->getBonus());
+var_dump($dirigente_1);
 ?>
